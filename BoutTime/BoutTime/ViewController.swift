17b7bfe271f48e1577f55be12f0906f8461f8b5a
yuhanli8909 <yuhan0921@qq.com>
2017-08-03 17:12:49 +0800

    created plist

--- BoutTime/BoutTime/ViewController.swift
+++ BoutTime/BoutTime/ViewController.swift
@@ -10,12 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayQuestion: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-    
-    @IBOutlet weak var displayEvent: UILabel!
     
     
 
